182c50fbf3fd0c27892c1f94aaf2f6abd14abb07
codemonkey-uk <codemonkey.uk@gmail.com>
2014-03-22 14:04:31 +0000

    added rotation from euler

--- matrix4.h
+++ matrix4.h
@@ -52,6 +52,9 @@ namespace Geometry
 
         static Matrix4 RotationAroundZ(Scalar r);
         void BecomeRotationAroundZ(Scalar r);
+        
+        static Matrix4 RotationFromEuler(const VectorN<Scalar,3>& r);
+        void BecomeRotationFromEuler(const VectorN<Scalar,3>& r);
     };
 
     //
@@ -185,7 +188,43 @@ namespace Geometry
 		this->mData[14] = 0;
 		this->mData[15] = 1;
 	}
-		
+	
+	// static 
+	template< typename Scalar>
+	Matrix4<Scalar> Matrix4<Scalar>::RotationFromEuler(const VectorN<Scalar,3>& r)
+	{
+		Matrix4<Scalar> result(uninitialised);
+		result.RotationFromEuler(r);
+		return result;
+	}
+	
+	template< typename Scalar>
+    void Matrix4<Scalar>::BecomeRotationFromEuler(const VectorN<Scalar,3>& r)
+    {
+		Scalar A = Cos(r.Get(0));
+		Scalar B = Sin(r.Get(0));
+		Scalar C = Cos(r.Get(1));
+		Scalar D = Sin(r.Get(1));
+		Scalar E = Cos(r.Get(2));
+		Scalar F = Sin(r.Get(2));
+
+		Scalar AD      =   A * D;
+		Scalar BD      =   B * D;
+
+		this->mData[0]  =   C * E;
+		this->mData[1]  =  -C * F;
+		this->mData[2]  =   D;
+		this->mData[4]  =  BD * E + A * F;
+		this->mData[5]  = -BD * F + A * E;
+		this->mData[6]  =  -B * C;
+		this->mData[8]  = -AD * E + B * F;
+		this->mData[9]  =  AD * F + B * E;
+		this->mData[10] =   A * C;
+
+		this->mData[3]  =  this->mData[7] = this->mData[11] = this->mData[12] = this->mData[13] = this->mData[14] = 0;
+		this->mData[15] =  1;
+    }
+    
 }//namespace Geometry
 
 #endif
