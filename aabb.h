3e06f00271ef8e950edc647558d5a090cc3bb07e
codemonkey-uk <codemonkey.uk@gmail.com>
2014-11-08 09:56:10 +0000

    AABB work: arbitary dimention GetVolume added, and more tidy up

--- aabb.h
+++ aabb.h
@@ -14,36 +14,39 @@ namespace Geometry
     {
         public:
             typedef T VectorType;
+            typedef typename VectorType::ScalarType ScalarType;
             typedef typename VectorType::BaseType VectorBase;
-            
+
             AxisAlignedBoundingBox(const VectorType& minBound, const VectorType& maxBound);
             AxisAlignedBoundingBox(const VectorType& origin);
-            AxisAlignedBoundingBox(const Uninitialised&); 
+            AxisAlignedBoundingBox(const Uninitialised&);
 
             const VectorType& GetMinBound() const;
             const VectorType& GetMaxBound() const;
             void SetMinBound(const VectorBase&);
             void SetMaxBound(const VectorBase&);
-            
+
             VectorType GetCenter() const;
             bool Contains( const VectorBase& p ) const;
             void ExpandToContain( const VectorBase& p );
-            
-            typename VectorType::ScalarType GetAxisExtent(size_t axis) const;
+
+            ScalarType GetAxisExtent(size_t axis) const;
             VectorType GetDiagonal() const;
-            
+
+            ScalarType GetVolume()const;
+
             void Move(const VectorBase& direction);
             void MoveMinBound(const VectorBase& direction);
             void MoveMaxBound(const VectorBase& direction);
-            
-            typename VectorType::ScalarType GetInradius()const;
+
+            ScalarType GetInradius()const;
             VectorType GetIncenter()const;
-            
+
             bool Overlaps(const AxisAlignedBoundingBox& rhs)const;
-            
+
             //compare sizes of AABBs
             bool CanContain( const AxisAlignedBoundingBox& rhs )const;
-            
+
         protected:
             VectorType mA,mB;
     };
@@ -52,60 +55,60 @@ namespace Geometry
     // Class Implementation
     // (in header as is a template)
     //
-    
+
     template<typename T>
     AxisAlignedBoundingBox<T>::AxisAlignedBoundingBox(const VectorType& minBound, const VectorType& maxBound)
-        : mA(minBound), mB(maxBound) 
-    { 
+        : mA(minBound), mB(maxBound)
+    {
         //assert(mA <= mB);
     }
 
     template<typename T>
     AxisAlignedBoundingBox<T>::AxisAlignedBoundingBox(const VectorType& o)
-        : mA(o), mB(o) 
+        : mA(o), mB(o)
     {
     }
-    
+
     template<typename T>
     AxisAlignedBoundingBox<T>::AxisAlignedBoundingBox(const Uninitialised&)
         : mA(uninitialised), mB(uninitialised)
     {
     }
-    
+
     template<typename T>
     const typename AxisAlignedBoundingBox<T>::VectorType& AxisAlignedBoundingBox<T>::GetMinBound() const
     {
         return mA;
     }
-    
+
     template<typename T>
     const typename AxisAlignedBoundingBox<T>::VectorType& AxisAlignedBoundingBox<T>::GetMaxBound() const
     {
         return mB;
     }
-    
+
     template<typename T>
     void AxisAlignedBoundingBox<T>::SetMinBound(const VectorBase& p)
     {
         mA = p;
         //assert(mA <= mB);
     }
-    
+
     template<typename T>
     void AxisAlignedBoundingBox<T>::SetMaxBound(const VectorBase& p)
     {
         mB = p;
-        //assert(mA <= mB);        
+        //assert(mA <= mB);
     }
 
     template<typename T>
-    typename AxisAlignedBoundingBox<T>::VectorType AxisAlignedBoundingBox<T>::GetCenter() const 
+    typename AxisAlignedBoundingBox<T>::VectorType AxisAlignedBoundingBox<T>::GetCenter() const
     {
         VectorType result(uninitialised);
         ComputeMidpoint(mA,mB,&result);
         return result;
     }
-    
+
     template<typename T>
     bool AxisAlignedBoundingBox<T>::Contains( const VectorBase& p ) const
     {
@@ -126,43 +129,54 @@ namespace Geometry
             if (mB[d] < p[d]) mB[d] = p[d];
         }
     }
-    
+
     template<typename T>
-    typename AxisAlignedBoundingBox<T>::VectorType::ScalarType AxisAlignedBoundingBox<T>::GetAxisExtent(size_t axis) const
+    typename AxisAlignedBoundingBox<T>::ScalarType AxisAlignedBoundingBox<T>::GetAxisExtent(size_t axis) const
     {
         return mB[axis] - mA[axis];
     }
-    
+
+    template<typename T>
+    typename AxisAlignedBoundingBox<T>::ScalarType AxisAlignedBoundingBox<T>::GetVolume()const
+    {
+        ScalarType result = GetAxisExtent(0);
+        for (size_t d = 1; d!=VectorBase::sDimensions; ++d)
+        {
+            result *= GetAxisExtent(d);
+        }
+        return result;
+    }
+
     template<typename T>
     typename AxisAlignedBoundingBox<T>::VectorType AxisAlignedBoundingBox<T>::GetDiagonal() const
     {
         return typename AxisAlignedBoundingBox<T>::VectorType(mB - mA);
     }
-    
+
     template<typename T>
     void AxisAlignedBoundingBox<T>::Move(const VectorBase& direction)
     {
         mA += direction;
         mB += direction;
     }
-    
+
     template<typename T>
     void AxisAlignedBoundingBox<T>::MoveMinBound(const VectorBase& direction)
     {
             mA += direction;
-    }    
-    
+    }
+
     template<typename T>
     void AxisAlignedBoundingBox<T>::MoveMaxBound(const VectorBase& direction)
     {
             mB += direction;
     }
-    
+
     template<typename T>
-    typename AxisAlignedBoundingBox<T>::VectorType::ScalarType AxisAlignedBoundingBox<T>::GetInradius()const
+    typename AxisAlignedBoundingBox<T>::ScalarType AxisAlignedBoundingBox<T>::GetInradius()const
     {
-        typename AxisAlignedBoundingBox<T>::VectorType::ScalarType a,b;
-        a = GetAxisExtent(0); 
+        ScalarType a,b;
+        a = GetAxisExtent(0);
         for (size_t d = 1; d!=VectorBase::sDimensions; ++d)
         {
             b = GetAxisExtent(d);
@@ -170,13 +184,13 @@ namespace Geometry
         }
         return a/2;
     }
-    
+
     template<typename T>
     typename AxisAlignedBoundingBox<T>::VectorType AxisAlignedBoundingBox<T>::GetIncenter()const
     {
         return GetCenter();
     }
-    
+
     template<typename T>
     bool AxisAlignedBoundingBox<T>::Overlaps(const AxisAlignedBoundingBox<T>& rhs)const
     {
@@ -187,7 +201,7 @@ namespace Geometry
         }
         return true;
     }
-    
+
     template<typename T>
     bool AxisAlignedBoundingBox<T>::CanContain(const AxisAlignedBoundingBox<T>& rhs)const
     {
--- aabb2d.h
+++ aabb2d.h
@@ -18,65 +18,64 @@ namespace Geometry
         public:
             typedef AxisAlignedBoundingBox< Vector2d<Scalar> > BaseType;
             typedef typename BaseType::VectorType VectorType;
-            
+
             AxisAlignedBoundingBox2d(const VectorType& minBound, const VectorType& maxBound)
-                : BaseType( minBound, maxBound ) 
+                : BaseType( minBound, maxBound )
             { }
-            
+
             AxisAlignedBoundingBox2d(Scalar x1, Scalar y1, Scalar x2, Scalar y2)
-                : BaseType( VectorType(x1, y1), VectorType(x2, y2) ) 
+                : BaseType( VectorType(x1, y1), VectorType(x2, y2) )
             { }
-            
+
             AxisAlignedBoundingBox2d(const Uninitialised& u)
              : BaseType(u) {}
-             
+
             AxisAlignedBoundingBox2d(const BaseType& rhs)
              : BaseType(rhs) {}
 
             AxisAlignedBoundingBox2d(const VectorType& rhs)
              : BaseType(rhs) {}
-                                       
+
             typename VectorType::ScalarType GetWidth() const
             {
                 return this->GetAxisExtent(0);
             }
-            
+
             typename VectorType::ScalarType GetHeight() const
             {
                 return this->GetAxisExtent(1);
             }
-            
-            // TODO: Create general GetVolume in base
-			typename VectorType::ScalarType GetArea()const{
-				return GetWidth()*GetHeight();
-			}
-			
-			Triangle2d<Scalar> GetEnclosingTriangle()const
-			{
-				typename Triangle2d<Scalar>::VertexType
-				    a( this->mA.GetX() + this->GetWidth()/2, this->mA.GetY() - this->GetWidth() ), 
-					b( this->mB.GetX() + this->GetHeight(),  this->mB.GetY() ), 
-					c( this->mA.GetX() - this->GetHeight(),  this->mB.GetY() );
-				return Triangle2d<Scalar>( a, b, c );
-			}
-						
-			VectorType BottomLeft()const
-			{
-				return this->mA;
-			}
-			VectorType BottomRight()const
-			{
-				return VectorType(this->mB.GetX(),this->mA.GetY());
-			}
-			VectorType TopLeft()const
-			{
-				return VectorType(this->mA.GetX(),this->mB.GetY());
-			}
-			VectorType TopRight()const
-			{
-				return this->mB;
-			}			
+
+            typename VectorType::ScalarType GetArea()const{
+                return this->GetVolume();
+            }
+
+            Triangle2d<Scalar> GetEnclosingTriangle()const
+            {
+                typename Triangle2d<Scalar>::VertexType
+                    a( this->mA.GetX() + this->GetWidth()/2, this->mA.GetY() - this->GetWidth() ),
+                    b( this->mB.GetX() + this->GetHeight(),  this->mB.GetY() ),
+                    c( this->mA.GetX() - this->GetHeight(),  this->mB.GetY() );
+                return Triangle2d<Scalar>( a, b, c );
+            }
+
+            VectorType BottomLeft()const
+            {
+                return this->mA;
+            }
+            VectorType BottomRight()const
+            {
+                return VectorType(this->mB.GetX(),this->mA.GetY());
+            }
+            VectorType TopLeft()const
+            {
+                return VectorType(this->mA.GetX(),this->mB.GetY());
+            }
+            VectorType TopRight()const
+            {
+                return this->mB;
+            }
     };
-}    
+}
 
 #endif//GEOMETRY_AABB2D_H_INCLUDED_
